acadb449f35cc0e7d66a06b48c9b7344bed732b0
Arafatun Nahar <arafatunnahar@Arafatuns-Air.lan>
2026-02-22 01:44:15 -0500

    bind render port

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ def prayer_times():
     })
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
